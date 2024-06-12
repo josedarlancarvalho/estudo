2a07ce9660e954af06d07ac2b4944a33008704ed
Darlan Carvalho <137117083+josedarlancarvalho@users.noreply.github.com>
2024-06-12 09:06:21 -0300

    Add files via upload

---
+++ python 01/1_operacores_aritimeticos.py
@@ -0,0 +1,22 @@
+#operadores aritmeticos
+
+produto_1 = 5
+produto_2 = 12    
+
+print(produto_1 + produto_2)
+print(produto_2 - produto_1)
+print(produto_1 * produto_2)
+
+x = 10 - 5
+
+print ((x + produto_1) - produto_2) 
+
+
+#o python respeita a regra da matematica, ou seja, ele segue a ordem da matematica.
+#Parênteses: ()
+#Exponentiação: **
+#Sinais positivos e negativos: +x, -
+#xMultiplicação, Divisão, Divisão inteira e Módulo: *, /, //, %
+#Adição e Subtração: +, -Operadores de comparação: ==, !=, >, <, >=, <=
+#Operadores lógicos: not, and, or
+
---
+++ python 01/2_operadores_comparacao.py
@@ -0,0 +1,10 @@
+#operadores de comparacao
+
+saldo = 10
+saque = 20
+
+print (saldo == saque)
+print (saque != saldo)
+print (saldo > saque)
+print (saldo >= saque)
+print (saldo < saque)
